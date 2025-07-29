5a527c4f7c78b87046ed37afea9a70e38ea50584
Jubin Lirawi <jubin.lirawi@d-fine.com>
2025-07-29 17:10:10 +0200

    replaced a fixture with a parametrized fixture

--- tests/test_time_record_line_to_dict.py
+++ tests/test_time_record_line_to_dict.py
@@ -5,31 +5,48 @@ import pytest
 import time_recorder as tr
 
 
-@pytest.fixture
-def line() -> tr.TimeRecorder:
-    """Fixture to create a sample TimeRecorder for time_report_line_to_dict tests."""
-    return tr.TimeRecorder(
-        date="24.04.2025",
-        start_time="08:00",
-        end_time="16:00",
-        lunch_break_duration=30,
-    )
-
-
+@pytest.mark.parametrize(
+    ("line", "expected_result"),
+    [
+        (
+            tr.TimeRecorder(
+                date="24.04.2025",
+                start_time="08:00",
+                end_time="16:00",
+                lunch_break_duration=30,
+            ),
+            {
+                "weekday": "Thu",  # 24.04.2025 is a Thursday
+                "date": "24.04.2025",
+                "start_time": "08:00:00",
+                "end_time": "16:00:00",
+                "lunch_break_duration": 30,
+            },
+        ),
+        (
+            tr.TimeRecorder(
+                date="29.07.2025",
+                start_time="07:00",
+                end_time="17:20",
+                lunch_break_duration=60,
+            ),
+            {
+                "weekday": "Tue",
+                "date": "29.07.2025",
+                "start_time": "07:00:00",
+                "end_time": "17:20:00",
+                "lunch_break_duration": 60,
+            },
+        ),
+    ],
+)
 @pytest.mark.fast
-def test_time_report_line_to_dict_returns_expected_dict(line: tr.TimeRecorder) -> None:
+def test_time_report_line_to_dict_returns_expected_dict(line: tr.TimeRecorder, expected_result: dict) -> None:
     """Test that time_report_line_to_dict returns a dictionary with expected keys and values."""
     result = line.time_report_line_to_dict()
-    results = {
-        "weekday": "Thu",  # 24.04.2025 is a Thursday
-        "date": "24.04.2025",
-        "start_time": "08:00:00",
-        "end_time": "16:00:00",
-        "lunch_break_duration": 30,
-    }
     assert isinstance(result, dict)
     assert set(result.keys()) == {"weekday", "date", "start_time", "end_time", "lunch_break_duration", "work_time", "case", "overtime"}
-    for key, value in results.items():
+    for key, value in expected_result.items():
         assert result[key] == value
     assert isinstance(result["work_time"], float)
     assert result["case"] in {"overtime", "undertime"}
